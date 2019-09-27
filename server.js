fe84c9a5c5e49ecfa3fe3d5864dac466eee4f854
davidpom <davidpom@gmail.com>
2019-09-27 18:25:57 -0400

    continuing db problems

--- server.js
+++ server.js
@@ -26,7 +26,30 @@ app.use(routes);
 
 // Connect to the Mongo DB
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
-mongoose.connect(process.env.MONGODB_URI || "mongodb://bookUser:Password1@ds023644.mlab.com:23644/heroku_krq6gtlx");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://bookUser:Password1@ds023644.mlab.com:23644/heroku_krq6gtlx");
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://bookUser:Password1@ds023644.mlab.com:23644/heroku_krq6gtlx";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {  
+console.log('Mongoose default connection error: ' + err);
+}); 
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {  
+console.log('Mongoose default connection disconnected'); 
+});
+
+// If the Node process ends, close the Mongoose connection 
+process.on('SIGINT', function() {  
+mongoose.connection.close(function () { 
+    console.log('Mongoose default connection disconnected through app termination'); 
+    process.exit(0); 
+}); 
+}); 
+
 
 // Send every other request to the React app
 // Define any API routes before this runs
