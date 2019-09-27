132d70e8eea4063828f44af56c483a3db4af312a
davidpom <davidpom@gmail.com>
2019-09-27 17:57:26 -0400

    build2

---
+++ client/build/precache-manifest.03d1dca54598a38b12eea0b8295f1a98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3f1ed46202430c2903bc",
+    "url": "/static/js/main.60e4f389.chunk.js"
+  },
+  {
+    "revision": "31429f97380497e47ab8",
+    "url": "/static/js/2.6f75d0e5.chunk.js"
+  },
+  {
+    "revision": "3f1ed46202430c2903bc",
+    "url": "/static/css/main.151e5170.chunk.css"
+  },
+  {
+    "revision": "ff45598f606b4ddbc814644d417d4280",
+    "url": "/index.html"
+  }
+];
